2d367b43fea365d1484fc00669c6918484ed84dc
sahlet <sahlet82@gmail.com>
2020-05-06 14:10:23 +0200

    exceptions tested

--- EvaTest/EvaTest/DiscountSplitter.cpp
+++ EvaTest/EvaTest/DiscountSplitter.cpp
@@ -1,13 +1,24 @@
 #include "DiscountSplitter.h"
 
-#ifdef MINIMIZE_ROW_NUMBER
 void splitByDiscount(std::list<records::CheckRow>& check)
 {
-	
-}
-#else
-void splitByDiscount(std::list<records::CheckRow>& check)
-{
+	for (auto& row : check)
+	{
+		if (row.count == 0)
+		{
+			throw std::exception("the count is 0");
+		}
+
+		if (row.priceForOne == 0)
+		{
+			throw std::exception("the price is 0");
+		}
+
+		if (row.count * row.priceForOne < row.sumDiscount)
+		{
+			throw std::exception("discount is greater than price");
+		}
+	}
+
 
 }
-#endif
--- EvaTest/EvaTest/DiscountSplitter.h
+++ EvaTest/EvaTest/DiscountSplitter.h
@@ -2,6 +2,4 @@
 #include <list>
 #include "Records.h"
 
-#define MINIMIZE_ROW_NUMBER
-
 void splitByDiscount(std::list<records::CheckRow>& check);
